bbc364779d2e1989492280b6dd48dbc43281a26b
shaaanuu <monshan50@gmail.com>
2026-03-25 16:10:01 +0530

    feat: moved js into seperate file

---
+++ script.js
@@ -0,0 +1,11 @@
+// Display current time and date
+const now = new Date();
+const timeElement = document.getElementById("current-time");
+
+timeElement.textContent = now.toLocaleString("en-US", {
+  month: "short",
+  day: "numeric",
+  hour: "numeric",
+  minute: "2-digit",
+  hour12: true
+});
